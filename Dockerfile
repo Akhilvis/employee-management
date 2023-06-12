d580b4e1fed034c1d209c07bacf516fe80171c9b
Akhilvis <akhilviswam000@gmail.com>
2023-06-11 23:11:36 -0400

    bat file added

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ FROM python:3
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 WORKDIR /code
-COPY requirements.txt /code/
-RUN pip install -r requirements.txt
 COPY . /code/
+# RUN command pipenv shell & python cuea/manage.py runserver 0.0.0.0
---
+++ run_django.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM Activate the virtual environment (if you're using one)
+REM Replace 'C:\path\to\venv' with the actual path to your virtual environment
+call pipenv shell
+
+REM Navigate to the Django project directory
+REM Replace 'C:\path\to\project' with the actual path to your Django project
+cd C:\path\to\project
+
+REM Run Django migrations
+python cuea\manage.py migrate
+
+REM Start the Django development server
+python cuea\manage.py runserver
